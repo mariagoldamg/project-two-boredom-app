c88a4a2441ab78b07668263ae7dc54cd9bebb9de
mariagoldamg <122532237+mariagoldamg@users.noreply.github.com>
2023-02-02 10:40:55 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(()=>{
     const getAdvice = async()=>{
-     const response = await fetch ('http://www.boredapi.com/api/activity/');
+     const response = await fetch ('https://www.boredapi.com/api/activity/');
      const data = await response.json()
     setBored (data.activity)
       }
